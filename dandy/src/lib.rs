be76ddb5326541387e69d95a6be88bbeca8a6eca
Jonathan Widén <jonathan.loke.widen@gmail.com>
2024-01-16 00:31:31 +0100

    Removed template

--- dandy/src/lib.rs
+++ dandy/src/lib.rs
@@ -2,18 +2,3 @@ pub mod dfa;
 pub mod nfa;
 pub mod parser;
 mod table;
-
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
