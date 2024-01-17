7f8e4bdaec07ea7054e79aa5ee7956a3d421393b
Jonathan Widén <jonathan.loke.widen@gmail.com>
2024-01-17 04:29:48 +0100

    Add a basic cli

--- dandy-cli/src/main.rs
+++ dandy-cli/src/main.rs
@@ -1,4 +1,180 @@
+use std::fmt::Display;
+use std::fs;
+use std::path::PathBuf;
+use clap::{Parser, ValueEnum};
 use dandy::dfa::Dfa;
+use dandy::nfa::Nfa;
+use dandy::parser;
+use dandy::parser::ParsedDfa;
+
+/// This command can be used to run operations on DFAs or NFAs.
+///
+/// `--type dfa/nfa` specifies if you are testing DFAs or NFAs. Defaults to DFA.
+/// `--condition all/any` specifies if all lines or just any lines of the files needs to be accepted by the DFA
+///     when testing it.
+/// The `equivalence` operation checks which of many automata are equivalent to the automata provided.
+/// The `test` operation checks which files has all/any lines accepted by the automata.
+// Example usage: dandy equivalence tests/dfa1.dfa tests/example_tree/**/*.dfa
+#[derive(Parser, Debug)]
+#[command(version, author = "Jonathan Widén", about = "A cli tool for importing and checking DFAs and NFAs", long_about)]
+struct Args {
+    #[arg(short, long, value_enum, default_value_t)]
+    r#type: FaType,
+    #[arg(short, long, value_enum, default_value_t)]
+    condition: TestType,
+    #[arg()]
+    operation: OpType,
+    #[arg()]
+    automata: PathBuf,
+    #[arg()]
+    files: Vec<PathBuf>,
+}
+
+#[derive(Default, Clone, Copy, Debug, ValueEnum)]
+enum FaType {
+    #[default]
+    Dfa,
+    Nfa,
+}
+
+#[derive(Clone, Copy, Debug, ValueEnum)]
+enum OpType {
+    Equivalence,
+    Test,
+}
+
+#[derive(Default, Clone, Copy, Debug, ValueEnum)]
+enum TestType {
+    #[default]
+    All,
+    Any,
+}
+
+fn main() {
+    let args = Args::parse();
+    let file = fs::read_to_string(&args.automata);
+    match file {
+        Err(e) => {
+            eprintln!("Error reading input file: {e}");
+        }
+        Ok(f) => {
+            match args.operation {
+                OpType::Equivalence => {
+                    equivalence(&args, &f);
+                }
+                OpType::Test => {
+                    test(&args, &f);
+                }
+            }
+        }
+    }
+}
+
+enum EquivalenceResult {
+    FailedToRead(String),
+    FailedToParse(String),
+    FailedToValidate(String),
+    NotEquivalent,
+    Equivalent,
+}
+
+impl Display for EquivalenceResult {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        use EquivalenceResult::*;
+        match self {
+            FailedToRead(s) => write!(f, "Failed to read ({s})"),
+            FailedToParse(s) => write!(f, "Failed to parse ({s})"),
+            FailedToValidate(s) => write!(f, "Failed to validate ({s})"),
+            NotEquivalent => write!(f, "Not Equivalent"),
+            Equivalent => write!(f, "Equivalent")
+        }
+    }
+}
+
+fn equivalence(args: &Args, file: &str) {
+    use EquivalenceResult::*;
+    match args.r#type {
+        FaType::Dfa => {
+            let dfa: Dfa = {
+                let parse = parser::dfa(file);
+                if let Err(e) = parse {
+                    eprintln!("Error parsing input DFA: {e}");
+                    return;
+                }
+                let dfa = parse.unwrap().try_into();
+                if let Err(e) = dfa {
+                    eprintln!("Error validating input DFA: {e}");
+                    return;
+                }
+                dfa.unwrap()
+            };
+            let verify_fn = move |file: &str| -> EquivalenceResult {
+                let parse: ParsedDfa = match parser::dfa(file) {
+                    Err(e) => return FailedToParse(e.to_string()),
+                    Ok(f) => f
+                };
+                let other_dfa: Dfa = match Dfa::try_from(parse) {
+                    Err(e) => return FailedToValidate(e.to_string()),
+                    Ok(f) => f
+                };
+                if dfa.equivalent_to(&other_dfa) {
+                    Equivalent
+                } else {
+                    NotEquivalent
+                }
+            };
+            do_equivalence_check(args, verify_fn);
+        }
+        FaType::Nfa => {
+            let nfa: Nfa = {
+                let parse = parser::nfa(file);
+                if let Err(e) = parse {
+                    eprintln!("Error parsing input NFA: {e}");
+                    return;
+                }
+                let nfa = parse.unwrap().try_into();
+                if let Err(e) = nfa {
+                    eprintln!("Error validating input NFA: {e}");
+                    return;
+                }
+                nfa.unwrap()
+            };
+            let verify_fn = move |file: &str| -> EquivalenceResult {
+                let parse = match parser::nfa(file) {
+                    Err(e) => return FailedToParse(e.to_string()),
+                    Ok(f) => f
+                };
+                let other_nfa = match Nfa::try_from(parse) {
+                    Err(e) => return FailedToValidate(e.to_string()),
+                    Ok(f) => f
+                };
+                if nfa.equivalent_to(&other_nfa) {
+                    Equivalent
+                } else {
+                    NotEquivalent
+                }
+            };
+            do_equivalence_check(args, verify_fn);
+        }
+    }
+}
+
+fn do_equivalence_check(args: &Args, verify_fn: impl Fn(&str) -> EquivalenceResult) {
+    let results = args.files.iter().map(|file|
+        match fs::read_to_string(file) {
+            Err(e) => EquivalenceResult::FailedToRead(e.to_string()),
+            Ok(f) => verify_fn(&f)
+        }
+    ).zip(args.files.iter()).collect::<Vec<_>>();
+
+    results.iter().for_each(|(res, path)|
+        println!("{}: {}", path.to_string_lossy(), res.to_string())
+    )
+}
+
+fn test(args: &Args, file: &str) {
+    todo!()
+}
 
 fn main1() {
     let raw_dfa = "
@@ -24,7 +200,7 @@ fn main1() {
     assert!(dfa.equivalent_to(&dfa2));
 }
 
-fn main() {
+fn main2() {
     println!("Hello, world!");
     let dfa = dandy::parser::dfa(include_str!("example.dfa"));
     dbg!(&dfa);
@@ -79,9 +255,9 @@ fn main() {
     println!("Converted: (power construction)");
     println!("{}", nfa2.to_dfa().to_table());
 
-    let eq_nfa_dfa = dandy::parser::dfa(include_str!("eq_example2_nfa.dfa"))
+    let eq_nfa_dfa = parser::dfa(include_str!("eq_example2_nfa.dfa"))
         .unwrap();
-    let eq_nfa_dfa: dandy::dfa::Dfa = eq_nfa_dfa.try_into().unwrap();
+    let eq_nfa_dfa: Dfa = eq_nfa_dfa.try_into().unwrap();
     println!("Other:");
     println!("{}", eq_nfa_dfa.to_table());
 
